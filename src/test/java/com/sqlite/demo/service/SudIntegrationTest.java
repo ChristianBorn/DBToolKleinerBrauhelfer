d1ed8c358e28773064215b7e73ced2d85ec062ef
ChristianBorn <chrisborn@hotmail.de>
2023-08-02 14:04:53 +0200

    Add integrationtests for SudService

---
+++ src/test/java/com/sqlite/demo/service/SudIntegrationTest.java
@@ -0,0 +1,24 @@
+package com.sqlite.demo.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SudIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getSud_ExpectEmptyList() throws Exception {
+        mockMvc.perform(get("/sud")).andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+}
