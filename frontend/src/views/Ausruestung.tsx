27fd336dec346a329acbee8a15fdd9c468d755f2
ChristianBorn <chrisborn@hotmail.de>
2023-09-19 15:54:36 +0200

    Reefactoring

--- frontend/src/views/Ausruestung.tsx
+++ frontend/src/views/Ausruestung.tsx
@@ -31,7 +31,6 @@ function Ausruestung() {
             <TableComponent title={"Ausrüstung"} objectsToDisplay={ausruestung}></TableComponent>
             <TableComponent title={"Geräte"} objectsToDisplay={geraete}></TableComponent>
         </React.Fragment>
-
     );
 }
 
