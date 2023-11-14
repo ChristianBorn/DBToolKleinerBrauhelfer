62c27f70b7549306d7c62e842bd825e30957aec4
ChristianBorn <chrisborn@hotmail.de>
2023-11-14 17:12:24 +0100

    BE: Return meaningful message from backend for PUT-Endpoints of Gebinde

---
+++ backend/src/main/java/com/sqlite/demo/model/gebinde/GebindeFormDTO.java
@@ -0,0 +1,13 @@
+package com.sqlite.demo.model.gebinde;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class GebindeFormDTO {
+
+    String name;
+
+    int number;
+}
